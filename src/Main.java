4c09185465b3bcd8916ce144144290ecf584f61b
sofia <sofiagarcia.2021p@gmail.com>
2024-08-26 09:43:37 -0600

    sofia

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList alumnos = Data.ListaDeAlumnos();
+
+        for (Object cadaObjeto : alumnos){
+            System.out.println(cadaObjeto);
+        }
+
+    }
+}
